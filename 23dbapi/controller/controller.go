826399743bf97ba7127f001ae6756ab26a86b196
Aniket Pal <aniketindian8@gmail.com>
2021-11-26 16:14:41 +0530

    Feat: Connect to momgoDB 🙏

---
+++ 23dbapi/controller/controller.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectString = ""
+const dbName = "namastenetflix"
+const colName = "watchlist"
+
+var collection *mongo.Collection
+
+// connect with db
+func init() {
+	//client option
+	clientOption := options.Client().ApplyURI(connectString)
+
+	//connect to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB connection success")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	//collection instance
+	fmt.Println("Collection instance is ready")
+}
