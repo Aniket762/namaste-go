0328e8482958a1b74f16e55666229e517d38cf45
Aniket Pal <aniketindian8@gmail.com>
2021-11-27 03:54:04 +0530

    Feat: Get all items from DB 🙏

--- 23dbapi/controller/controller.go
+++ 23dbapi/controller/controller.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/Aniket762/namaste-go/dbapi/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -96,4 +98,32 @@ func DeleteAllMovies() int64 {
 	return deleteResult.DeletedCount
 }
 
+// get all movies from db
+func GetAllMovies() []primitive.M{
+	cur, err := collection.Find(context.Background(),bson.M{})
+	if err != nil{
+		log.Fatal(err)
+	}
 
+	var movies []primitive.M 
+	
+	for cur.Next(context.Background()){
+		var movie bson.M
+		err := cur.Decode(&movie)
+		if err != nil{
+			log.Fatal(err)
+		}
+
+		movies = append(movies, movie)
+	}
+
+	defer cur.Close(context.Background())
+	return movies
+}
+
+// controllers
+func GetAllMoviesController(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type","application/json")
+ 	allMovies := GetAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
+}
