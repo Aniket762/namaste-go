8dd42fa7e91ce91ead3384f259b21769d59d2e87
Aniket Pal <aniketindian8@gmail.com>
2021-11-27 00:07:24 +0530

    Feat:Update and delete operations added 🙏

--- 23dbapi/controller/controller.go
+++ 23dbapi/controller/controller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/Aniket762/namaste-go/dbapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -48,3 +50,50 @@ func InsertOneMovie(movie model.Netflix)  {
 
 	fmt.Println("Inserted one movie in db with id ", inserted.InsertedID)
 }
+
+// update 1 record
+func UpdateOneMovie(movieId string)  {
+	// converting string into valid mongo id
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	// creating filter to find the exact movie
+	// everything in mongodb is bson
+	filter := bson.M{"_id":id}
+	update := bson.M{"$set":bson.M{"watched":true}}
+
+	result, err := collection.UpdateOne(context.Background(),filter,update)
+
+	if err !=nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ",result.ModifiedCount)
+
+}
+
+// delete 1 record
+func DeleteOneMovie(movieId string)  {
+	id , _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id":id}
+	deleteCount , _ := collection.DeleteOne(context.Background(),filter)
+	fmt.Println("Movie got delete with delete count: ",deleteCount)
+}
+
+// delete all records
+func DeleteAllMovies() int64 {
+	// empty params implies all
+	deleteResult , err := collection.DeleteMany(context.Background(), bson.M{})
+
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("The movies which were deleted: ",deleteResult)
+	fmt.Println("The number of movies deleted: ",deleteResult.DeletedCount)
+
+	return deleteResult.DeletedCount
+}
+
+
