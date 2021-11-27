5f3c4f0c46e2bd3879c3f61e5d73b6f3d8bfc6cc
Aniket Pal <aniketindian8@gmail.com>
2021-11-27 14:10:16 +0530

    Feat:Wait groups added 🙏

--- 25goroutines/main.go
+++ 25goroutines/main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 )
+
+// for multi threading we need wait groups
+var wg sync.WaitGroup // pointer
 
 func main()  {
 	// its concurrently works
 	go greeter("Getting into")
 	greeter("Go routines 🙏")
+
+	waitlist := []string{
+		"https://google.com",
+		"https://aniketpal.vercel.app",
+		"https://github.com",
+	}
+
+	// single thread operation
+	for _, website := range waitlist{
+		GetStatusCode(website)
+	}
+
+	// multi thread operation - go routines
+	// using waitgroups for time sync
+	for _, website := range waitlist{
+		go GetStatusCodeWait(website)
+		wg.Add(1)
+	}
+	wg.Wait()
 }
 
 func greeter(s string)  {
@@ -22,6 +46,29 @@ func greeter(s string)  {
 		time.Sleep(2 * time.Millisecond)
 		fmt.Println(s)
 	} 
+}
+
+// single thread
+func GetStatusCode(endpoint string)  {
+	res , err := http.Get(endpoint)
+
+	if err!= nil{
+		fmt.Println("Endpoint not reachable")
+	}else{
+		fmt.Printf("%d status code for %s\n",res.StatusCode,endpoint)
+	}
+}
+
+// multi thread
+func GetStatusCodeWait(endpoint string)  {
+	defer wg.Done()
+	res , err := http.Get(endpoint)
+
+	if err!= nil{
+		fmt.Println("Endpoint not reachable")
+	}else{
+		fmt.Printf("%d status code for %s\n",res.StatusCode,endpoint)
+	}
 }
 
 /*
@@ -40,8 +87,7 @@ Thread:
 Go Routines
 - Managed by Go Runtime
 - Flexible length 2KB
-
-
-
 */
+
+
 
