80061555928080df4968092fefc6a08c20f7de44
Aniket762 <aniketindian8@gmail.com>
2021-12-05 21:00:08 +0530

    Feat: Joke generator CLI init

--- clis/jokes/cmd/random.go
+++ clis/jokes/cmd/random.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -9,24 +12,57 @@ import (
 // randomCmd represents the random command
 var randomCmd = &cobra.Command{
 	Use:   "random",
-	Short: "Get random jokes in your terminal randomly",
-	Long:  `Get random jokes in your terminal with various other options randomly`,
+	Short: "Get a random  joke",
+	Long:  `This command fetches a random joke from api`,
 	Run: func(cmd *cobra.Command, args []string) {
 		getRandomJoke()
 	},
 }
 
+func init() {
+	rootCmd.AddCommand(randomCmd)
+}
+
 type Joke struct {
 	ID     string `json:"id"`
-	Joke   string `json:"json"`
-	Status string `json:"status"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
 }
 
 func getRandomJoke() {
-	fmt.Println("Get random joke 😁")
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(joke.Joke))
 }
 
-func init() {
-	rootCmd.AddCommand(randomCmd)
+func getJokeData(baseAPI string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet, //method
+		baseAPI,        //url
+		nil,            //body
+	)
 
+	if err != nil {
+		panic(err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return responseBytes
 }
